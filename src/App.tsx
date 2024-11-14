dc35707f78662a67598ed1a30f28f15c4803dc98
Fdardev <Fransdavid.aguirrerueda@emeal.nttdata.com>
2024-11-14 09:11:06 -0500

    Error bunDary /buggyComoponent

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client'
 // import Errors from './components/Errors'
 // import NotFound from './components/pages/NotFound'
 import Unavailable from './components/pages/Unavailable'
+import ErrorBoundary from './components/ErrorBoundary'
+// import BuggyComponent from './components/BuggyComponent'
 
 const App = () => (
+  <ErrorBoundary>
   <div>
     {/* <Errors/> */}
+    {/* <BuggyComponent/> */}
     {/* <NotFound/> */}
+    {/* <h1>App</h1> */}
     <Unavailable/>.
   </div>
+  </ErrorBoundary>
 )
 const rootElement = document.getElementById('app')
 if (!rootElement) throw new Error('Failed to find the root element')
